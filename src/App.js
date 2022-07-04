7885f8fb3cff172498e526060aa184cbfb10dbc3
Adam Cherry <adam@wearewibble.com>
2022-07-04 11:43:30 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-full h-screen flex items-center justify-center text-center">
+        <h1 className="text-blue-800 font-bold font-sans text-5xl">Portfolio</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
