10b4e377959b2df387ed9617a24436be2e39af05
aacherry <aacherry@outlook.com>
2022-07-07 11:26:05 +0100

    Push to develop

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="w-full h-screen flex items-center justify-center text-center">
-        <h1 className="text-blue-800 font-bold font-sans text-5xl">Portfolio</h1>
+        <h1 className="text-blue-800 font-bold font-sans text-5xl">Portfolio: Develop Branch</h1>
       </div>
     </div>
   );
