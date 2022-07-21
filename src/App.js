116ce2a3a8d9dbd8053e1975edf2c5ab2cb8f3f7
Adam Cherry <adam.cherry@flintstudios.co.uk>
2022-07-21 16:46:58 +0100

    About us component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/App.css";
 
+import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
       <div className="w-full h-screen flex items-center justify-center text-center">
-        <h1 className="text-blue-800 font-bold font-sans text-5xl">Portfolio: Develop Branch</h1>
+        <AboutUs />
       </div>
     </div>
   );
---
+++ src/components/AboutSection.js
@@ -0,0 +1,30 @@
+import home1 from '../img/home1.png';
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span>
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>come true.</h2>
+          </div>
+        </div>
+        <p>Contact us for any photography and videography ideas you may have. We have amazing skills.</p>
+        <button>Contact us</button>
+      </div>
+      <div className="image">
+        <img src={home1} alt="Homepage image" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
