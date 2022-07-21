53389a53a337af70b05ba4ec501a7ed1452c4716
Adam Cherry <adam.cherry@flintstudios.co.uk>
2022-07-21 17:01:22 +0100

    Services section

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
-      <div className="w-full h-screen flex items-center justify-center text-center">
         <AboutUs />
-      </div>
     </div>
   );
 }
---
+++ src/components/ServicesSection.js
@@ -0,0 +1,18 @@
+const ServicesSecton = () => {
+    return(
+        <div className="services">
+            <div className="description">
+                <h2>High <span>quality</span> services.</h2>
+
+                <div className="grid cols-2 gap-8">
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServicesSecton;
